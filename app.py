30f30bb5a6d4adfa9c769b2e404edca10e09cd30
Gunjan Gayal <gayalgunjan@gmail.com>
2026-04-05 11:32:17 +0530

    fixed port issue for render

--- app.py
+++ app.py
@@ -333,5 +333,8 @@ def upload():
 
     return render_template("upload.html")
 # ---------------- RUN ----------------
-if __name__=="__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
